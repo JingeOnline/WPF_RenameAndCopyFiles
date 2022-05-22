7c25cbaea00e7708f9967a1cfbc726a3b9945108
Jinge Zhang <jinge_windows@hotmail.com>
2022-05-22 22:24:04 +1000

    Add project files.

---
+++ WPF_RenameAndCopyFiles/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using WPF_RenameAndCopyFiles.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace WPF_RenameAndCopyFiles
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ WPF_RenameAndCopyFiles/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using HandyControl.Controls;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace WPF_RenameAndCopyFiles.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private int _StepIndex;
+        public int StepIndex
+        {
+            get { return _StepIndex; }
+            set { SetProperty(ref _StepIndex, value); }
+        }
+
+        public DelegateCommand<StepBar> PrevCommand { get; set; }
+        public DelegateCommand<StepBar> NextCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            PrevCommand = new DelegateCommand<StepBar>(Prev);
+            NextCommand = new DelegateCommand<StepBar>(Next);
+        }
+
+        private void Next(StepBar stepBar)
+        {
+            stepBar.Next();
+        }
+
+        private void Prev(StepBar stepBar)
+        {
+                stepBar.Prev();
+        }
+    }
+}
